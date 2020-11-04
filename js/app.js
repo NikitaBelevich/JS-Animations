ca59a5cdb784d723da052379c7830b30e153b708
Nikita Belevich <belevish_1998@mail.ru>
2020-11-04 23:26:13 +0300

    Animation 1 is done

--- js/app.js
+++ js/app.js
@@ -4,14 +4,13 @@
 const ball1 = document.querySelector('.animation1__ball');
 ball1.addEventListener('click', function() {
     animate({
-        duration: 3000,
+        duration: 2000,
         // временная функция исполнения анимации, может быть не только кривой Безье
         timing: function(timeFraction) {
             return makeEaseOut(bounce)(timeFraction);
         },
         draw(progress) { // функция отрисовки анимации, она рисует анимацию
-            ball1.style.marginLeft = progress * 200 + 'px';
-            ball1.style.transform = `rotate(${progress * 180}deg)`;
+            ball1.style.transform = `translateY(${progress * 205}px)`;
         }
     });
 });
